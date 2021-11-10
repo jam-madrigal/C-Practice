60a39a82339e69c4b7bea5f323cb36cf6b9ea9c0
jam-madrigal <jam.madrigal@gmail.com>
2021-11-10 14:13:03 -0800

    Add comment about namespaces

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// The namespace basically specifies the project that we're using. We are in the "Practice" project. More on what it's really doing later.
 namespace Practice
 {
     // The "internal keyword" here is an access modifier. In this case, it means access is limited to the current assembly. Assemblies are basically the "types and resources"; the compiled code; that make up the .exe and .dll files that make up a .NET application. An assembly can be built from multiple source code files in .NET or .NET framework. Having an internal access modifier here will be useful in allowing groups of components to cooperate privately, as they will not be exposed to the rest of the application code. You can imagine how not having this could create unintended errors or just make a mess of things in some cases. If you were to try to instantiate the internal class Program from this file in another file, it would produce an error. Changing it to "public" can make it accessible.
