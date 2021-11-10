c9396a7176c5d5c26806de16288cc421f978c5bc
jam-madrigal <jam.madrigal@gmail.com>
2021-11-10 14:10:48 -0800

    Add comment about access modifiers

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Practice
 {
+    // The "internal keyword" here is an access modifier. In this case, it means access is limited to the current assembly. Assemblies are basically the "types and resources"; the compiled code; that make up the .exe and .dll files that make up a .NET application. An assembly can be built from multiple source code files in .NET or .NET framework. Having an internal access modifier here will be useful in allowing groups of components to cooperate privately, as they will not be exposed to the rest of the application code. You can imagine how not having this could create unintended errors or just make a mess of things in some cases. If you were to try to instantiate the internal class Program from this file in another file, it would produce an error. Changing it to "public" can make it accessible.
+
+    // In most cases, according to a user on stackoverflow, an assembly corresponds and is equal to one file on the disk. "In a multifile assembly there would still be only one assembly manifest in a DLL or EXE and the MSIL code in multiple netmodule files." I will take this part with a grain of salt for now.
+
     internal class Program
     {
         static void Main(string[] args)
