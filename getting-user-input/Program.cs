8480a97ce4578073742daacb9c9944640c7c9261
jam-madrigal <jam.madrigal@gmail.com>
2021-11-14 13:42:30 -0800

    Add explanations of stoing user input as variables

--- getting-user-input/Program.cs
+++ getting-user-input/Program.cs
@@ -10,8 +10,18 @@ namespace getting_user_input
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            // Console.Write(); will print on the same line, instead of a new one like Console.WriteLine();, and this will let the user make sense of where their input is going in this case
+            Console.Write("Enter your name: ");
+            // Store the line of text the user enters in a variable
+            string name = Console.ReadLine();
+            // Ask for their class/profession, and store that in a variable as well
+            Console.Write("Enter your class/profession: ");
+            string userClass = Console.ReadLine(); 
 
+            // Write a greeting to the user based on the input and display it in the console
+            Console.Write("Hello " + name + " the " + userClass + ".");
+
+            // We can use Console.ReadLine(); to get user input
             Console.ReadLine();
         }
     }
