951c466ebf5a30386ff94eec49b3f6583954892a
jam-madrigal <jam.madrigal@gmail.com>
2021-11-14 13:21:36 -0800

    Add note about pairs of integers in operations

--- data-types/Program.cs
+++ data-types/Program.cs
@@ -18,8 +18,13 @@ namespace data_types
             int age = 28;
             // Decimal numbers are stored in 1 of 3 data types: float, double, or decimal. These get progressively more specific and accurate, in that order. Use decimal if you want to be really precise, for example. Like for money, or anything that has to be extremely exact. For now, we'll be focusing on double in initial C# practice, something of a middle-ground
             double gpa = 3.4;
+
+            // A quirk to be aware of with integers: Two integers will always return an integer back, so something like 5 / 2 would return 2 instead of 2.5 or a remainder (modulus operator %) instead convert one to a double by writing something like 5 / 2.0 and you will get 2.5 as the output
+
             // True or false statements are just as we knew 'em from JavaScript, booleans, declared as such
             bool isMagical = true;
+
+            Console.WriteLine("hello i am data types");
 
             Console.ReadLine();
         }
