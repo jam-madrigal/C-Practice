5e1f85eb90c6474cb2507b20e5cc9842109b5201
jam-madrigal <jam.madrigal@gmail.com>
2021-11-14 13:55:46 -0800

    Update calculator to take user input and add it together

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -15,6 +15,14 @@ namespace calculator
             // Of course, we can only add numbers together, strings would just concatenate so this conversion makes this operation possible: 
             Console.WriteLine(num + 6);
 
+            // Wait for a user to enter two numbers and store them in a variable. It will return an error if we use Console.ReadLine() alone because it will try to store a string as a number
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            // Return the sum on a new line
+            Console.WriteLine(num1 + num2);
 
 
             Console.ReadLine();
