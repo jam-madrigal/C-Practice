5791c5c606fb71efdb98506710f1b64171dcf307
jam-madrigal <jam.madrigal@gmail.com>
2021-11-14 13:51:01 -0800

    Add explanation of converting strings to numbers

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -9,7 +9,13 @@ namespace calculator
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            // Demonstrating how to convert a string to a number, there are many Convert methods
+            int num = Convert.ToInt32("24");
+            // Of course, we can only add numbers together, strings would just concatenate so this conversion makes this operation possible: 
+            Console.WriteLine(num + 6);
+
+
 
             Console.ReadLine();
         }
