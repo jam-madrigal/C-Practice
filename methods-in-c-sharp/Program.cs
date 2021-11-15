730a79f58872c0b082d79540e083b3ef9474c5b9
jam-madrigal <jam.madrigal@gmail.com>
2021-11-15 15:28:55 -0800

    Update sayHi() to take in a second parameter

--- methods-in-c-sharp/Program.cs
+++ methods-in-c-sharp/Program.cs
@@ -10,17 +10,21 @@ namespace methods_in_c_sharp
     {
         static void Main(string[] args)
         {
-            // Create a method to say hi to the user and call it, this has to be done outside of the static void Main() method. If the method requires a parameter, it will return an error if no string is passed through
+            // Create a method to say hi to the user and call it, this has to be done outside of the static void Main() method. If the method requires a parameter, it will return an error if no string is passed through.
             SayHi("Meta");
             Console.ReadLine();
         }
 
         // Where we create new methods
         // Use the keyword static (explanation to come later), then specify the "return type", this will be void which basically means this method will not return any information, a simpler kind of method, then we name the method. In C#, we generally use capital letters for the names.
-        static void SayHi(string name)
+
+        // For parameters, we specify the type of data and the name of the parameter, consecutive parameters are separated by commas
+        static void SayHi(string name, int age)
         {
             // All the code for the method iinside the curly braces, of course
             Console.WriteLine("Hello " + name);
+            Console.WriteLine("You are " + age + " years old");
+
 
         }
     }
