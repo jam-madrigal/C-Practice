b25fad57ad1ef851c64a99669cd7e22fc32d83f2
jam-madrigal <jam.madrigal@gmail.com>
2021-11-15 15:14:04 -0800

    Add example of creating an empty array and specifying the size

--- arrays-in-csharp/Program.cs
+++ arrays-in-csharp/Program.cs
@@ -14,6 +14,17 @@ namespace arrays_in_csharp
             // First specify the type of data the array will hold, that we're making an array instead of a variable with [], give it a name, and then initialize
             int[] luckyNumbers = { 3, 13, 24, 37, 52};
 
+            // An array of strings, and populating it differently, the number inside the initialization specifies how many elements we want to fit into the array eventually
+            string[] magicalTraits = new string[4];
+            // Populating the array
+            magicalTraits[0] = "Sorcery";
+            magicalTraits[1] = "Pointy hat";
+            magicalTraits[2] = "Musty tome";
+            magicalTraits[3] = "Crooked staff";
+
+            // Accessing one of the indexes of the array of strings
+            Console.WriteLine(magicalTraits[2]);
+
             // How to access an index of an array in C#, in this case the third index or 37
             Console.WriteLine(luckyNumbers[3]);
 
