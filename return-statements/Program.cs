485124cd64324d8dbd8b9e3e84b3d0dde45b5e30
jam-madrigal <jam.madrigal@gmail.com>
2021-11-16 17:36:24 -0800

    Update comments

--- return-statements/Program.cs
+++ return-statements/Program.cs
@@ -21,6 +21,7 @@ namespace return_statements
             Console.ReadLine();
         }
         // Creating another method that cubes a number, and returns the number, the second part of declaring this is the return type, this time instead of "void" we will specify a data type we are going to return, and the method will take in an integer as the variable "num"
+        // We can use other data types than int. We can return string, double, or even an array of doubles by using double[]
         static int cube(int num)
         {
             // Storing the num variable cubed in an integer variable called result
