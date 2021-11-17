35b42ca852fba1c157fc4f899eca5e4881b2475d
jam-madrigal <jam.madrigal@gmail.com>
2021-11-17 14:49:45 -0800

    Add GetDay method and input validation

--- switch-statements/Program.cs
+++ switch-statements/Program.cs
@@ -9,7 +9,19 @@ namespace switch_statements
     internal class Program
     {
         static void Main(string[] args)
+        { // Create a switch statement that returns the day of the week corresponding to the user input number
+
+            Console.ReadLine();
+        }
+
+        static string GetDay(int dayNum)
         {
+            // Check if the number is less than 0 or greater than 7, and if so return an error
+            if (dayNum < 0 || dayNum > 7)
+            {
+                Console.WriteLine("Invalid day number");
+            }
+
         }
     }
 }
