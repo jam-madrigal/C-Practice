efac1f58faa7a411ca913c1df566e81ebdd00177
jam-madrigal <jam.madrigal@gmail.com>
2021-11-16 17:32:03 -0800

    Add cube function and a call to it

--- return-statements/Program.cs
+++ return-statements/Program.cs
@@ -10,6 +10,19 @@ namespace return_statements
     {
         static void Main(string[] args)
         {
+            // Calling our cube function
+            cube(3);
+
+            Console.ReadLine();
         }
+        // Creating another method that cubes a number, and returns the number, the second part of declaring this is the return type, this time instead of "void" we will specify a data type we are going to return, and the method will take in an integer as the variable "num"
+        static int cube(int num)
+        {
+            // Storing the num variable cubed in an integer variable called result
+            int result = num * num * num;
+            // Returning the stored result and breaking us out of the method
+            return result;
+        }
+        
     }
 }
