e4da55306b8c37ba607956327474173b716e752b
jam-madrigal <jam.madrigal@gmail.com>
2021-11-17 14:33:57 -0800

    Add better calculator project

---
+++ better-calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace better_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // Create a calculator which prompts the user for two numbers and what kind of comparison or operation they want to do
+            // Prompt the user to enter a number and store that number in a variable. Remember to convert the response from a string to an integer.
+            Console.Write("Enter a number: ")
+            double num1 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+        }
+    }
+}
