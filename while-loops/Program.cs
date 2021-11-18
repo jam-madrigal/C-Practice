a3472ab384e1da980146e9baffaecde145e1ed53
jam-madrigal <jam.madrigal@gmail.com>
2021-11-18 14:09:48 -0800

    Add while loop project

---
+++ while-loops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // Finally getting more into the meat of it with some loops.. One of the most powerful tools in code
+
+
+            Console.ReadLine();
+        }
+    }
+}
