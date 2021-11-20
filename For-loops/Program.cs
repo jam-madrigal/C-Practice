e80d35101eefcae9360644b842fa77a48eed92f2
jam-madrigal <jam.madrigal@gmail.com>
2021-11-19 21:37:16 -0800

    Update for loop to loop through an array of numbers

--- For-loops/Program.cs
+++ For-loops/Program.cs
@@ -10,9 +10,13 @@ namespace For_loops
     {
         static void Main(string[] args)
         { // For loops follow this convention. This loop will increment i by one, stating with int i = 0, and will loop for as long as i is less than 5. First we have our initalized variable, the condition under which it will run, and a line of code executed after every iteration of the loop, in this case i++.
-            for (int i = 0; i < 5; i++)
+
+            // Loop through an array and print the current index of the array
+            int[] luckyNumbers = { 3, 6, 9, 13, 24, 37 };
+
+            for (int i = 0; i < luckyNumbers.Length; i++)
             {
-                Console.WriteLine(i);
+                Console.WriteLine(luckyNumbers[i]);
             }
 
 
