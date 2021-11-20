81c9a0ad284aad6ffc15ec4653a55b1dea471257
jam-madrigal <jam.madrigal@gmail.com>
2021-11-19 21:31:08 -0800

    Add example for loop

--- For-loops/Program.cs
+++ For-loops/Program.cs
@@ -9,7 +9,14 @@ namespace For_loops
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { // For loops follow this convention. This loop will increment i by one, stating with int i = 0, and will loop for as long as i is less than 5.
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
