904a99d0c9e29f38ffcb963ef37d7ff8bec99dde
jam-madrigal <jam.madrigal@gmail.com>
2021-11-20 12:00:26 -0800

    Add exception handling project

---
+++ exception-handling/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception_handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine(num1 / num2);
+
+            Console.ReadLine();
+        }
+    }
+}
