04d619ebdf07e8336aa8ba6e663570ecbdb0b3a7
jam-madrigal <jam.madrigal@gmail.com>
2021-11-21 14:47:21 -0800

    Add book class

---
+++ classes-and-objects/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_and_objects
+{
+    internal class Book
+    {
+        // We will create variables that reprsent all the attributes of a book. These are called class attributes.
+        public string title;
+        public string author;
+        public int pages;
+    }
+}
--- classes-and-objects/Program.cs
+++ classes-and-objects/Program.cs
@@ -9,7 +9,7 @@ namespace classes_and_objects
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { // Classes/objects can be used to represent things that are more complex than a single data type. They are like a complex data type we can create. Too create a class, right click our project in the solution explorer > Add > new item > class (name it with a capital letter, generally)
 
 
             Console.ReadLine();
