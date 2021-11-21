dd43b638a2b79995bd37581cb2c3daa2ec664747
jam-madrigal <jam.madrigal@gmail.com>
2021-11-21 15:37:43 -0800

    Refactor Book instances to use constructors

--- classes-and-objects/Program.cs
+++ classes-and-objects/Program.cs
@@ -12,28 +12,31 @@ namespace classes_and_objects
         { // Classes/objects can be used to represent things that are more complex than a single data type. They are like a complex data type we can create. Too create a class, right click our project in the solution explorer > Add > new item > class (name it with a capital letter, generally)
 
             // Here we can create a representation of a book using the Book class which defines the data type of "Book". An object is an instance of a class, and we can use objects to create a book in our main program.
-        /* Uncomment and use this example first, then comment it again for the constructors method of creating the same instances of Book
-         
-            // Creating a book object, or an instance of the class Book. This is like a variable that is storing a book object.
-            Book book1 = new Book("Miss");
-            // Initializing the properties of the book as defined in the class
-            book1.title = "The witch in the woods";
-            book1.author = "Meta";
-            book1.pages = 1337;
+            /* Uncomment and use this example first, then comment it again for the constructors method of creating the same instances of Book
 
-            Book book2 = new Book("Mister");
-            book2.title = "The bubbly cauldron";
-            book2.author = "Jack Pickleson";
-            book2.pages = 69;
+                // Creating a book object, or an instance of the class Book. This is like a variable that is storing a book object.
+                Book book1 = new Book("Miss");
+                // Initializing the properties of the book as defined in the class
+                book1.title = "The witch in the woods";
+                book1.author = "Meta";
+                book1.pages = 1337;
+
+                Book book2 = new Book("Mister");
+                book2.title = "The bubbly cauldron";
+                book2.author = "Jack Pickleson";
+                book2.pages = 69;
 
 
-            // Accessing and printing the book's title
-            Console.WriteLine(book1.title); */
+                // Accessing and printing the book's title
+                Console.WriteLine(book1.title); */
 
             // Creating the same code as above, but cleaner, by using constructors
-            
+            Book book1 = new Book("Miss", "The witch in the woods", "Meta", 1337);
+            Book book2 = new Book("Mister", "The bubbly cauldron", "Jack Pickleson", 69);
 
-
+            // Update any of the values simply by doing things like book2.title = "Humbug"
+            // Test that the classes/objects, the instances of Book, were created correctly
+            Console.WriteLine(book1.title, book2.title);
 
             Console.ReadLine();
         }
