ab8b52373e0bb20df95b2c519000cb2beb384b1e
jam-madrigal <jam.madrigal@gmail.com>
2021-11-21 15:24:44 -0800

    Add explanation of constructors

--- classes-and-objects/Book.cs
+++ classes-and-objects/Book.cs
@@ -12,5 +12,14 @@ namespace classes_and_objects
         public string title;
         public string author;
         public int pages;
+
+        // Let's create book instances another way, with constructors
+        // A method like this is a constructor, the method public Book()
+        // We can give the constructor the parameters to give the book properties and data just like we did above. Now we can pass in a parameter in our new Book() method
+        public Book(string name)
+        {
+            // Every time a book is created, this code will be ran. Try running the main program to see it print for as many books are created above. When we call new Book() in the main program we're actually calling the constructor method. It is directly linked to this method.
+            Console.WriteLine(name);
+        }
     }
 }
--- classes-and-objects/Program.cs
+++ classes-and-objects/Program.cs
@@ -14,16 +14,17 @@ namespace classes_and_objects
             // Here we can create a representation of a book using the Book class which defines the data type of "Book". An object is an instance of a class, and we can use objects to create a book in our main program.
 
             // Creating a book object, or an instance of the class Book. This is like a variable that is storing a book object.
-            Book book1 = new Book();
+            Book book1 = new Book("Miss");
             // Initializing the properties of the book as defined in the class
             book1.title = "The witch in the woods";
             book1.author = "Meta";
             book1.pages = 1337;
 
-            Book book2 = new Book();
+            Book book2 = new Book("Mister");
             book2.title = "The bubbly cauldron";
             book2.author = "Jack Pickleson";
             book2.pages = 69;
+
 
             // Accessing and printing the book's title
             Console.WriteLine(book1.title);
